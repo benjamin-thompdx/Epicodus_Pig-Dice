2b2169b0224101dce2c3dc0636b0c720181296f4
thom and kwicz <kwicz@>
2020-01-29 11:14:49 -0800

    update index.html with buttons for roll and hold, update scripts.js to include back-end logic for checkRoll function and nextRoll function, and update scripts.js front-end logic to include button #roll.click function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+// Business-Logic
+
+//
+function Game() {
+    this.turn = "player1";
+    this.player1Score = 0
+    this.player2Score = 0
+    this.points = [2,3,4,5,6];
+    this.tempScore = 0; 
+}
+
+function checkRoll(game, roll) {
+    if (game.points.includes(roll)) {
+        console.log(game.tempScore += roll);
+    } else {
+        nextTurn(game);
+    }
+
+}
+
+function nextTurn(game, tempScore) {
+    if (game.turn === "player1") {
+        if (tempScore) {
+            game.player1Score = tempScore;
+        }
+        game.turn = "player2"
+    } else {
+        if (tempScore) {
+            game.player2Score = tempScore;
+        }
+        game.turn = "player1"
+    } 
+    game.tempScore = 0
+    console.log(game, "game");
+}
+
+//User-Logic
+
 $(document).ready(function(){
+    var game = new Game()
+    $("#roll").click(function() {
+        var die = Math.floor((Math.random() *6) +1);
+        console.log(die)
+        checkRoll (game, die);
+    });
 
 });
